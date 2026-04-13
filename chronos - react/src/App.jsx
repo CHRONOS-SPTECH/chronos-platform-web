94be5a3ebd8e2e5043914cf8f0d98d4a0e1bb35e
FernandoRDev457 <fernando.gutierrez@sptech.school>
2026-04-13 18:08:03 -0300

    feat: Implementando Routes

--- chronos - react/src/App.jsx
+++ chronos - react/src/App.jsx
@@ -1,16 +1,18 @@
-import Login from "./pages/Presenca"
-import HomeInstrutor from "./pages/HomeInstrutor"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import HomeInstrutor from "./pages/HomeInstrutor";
 
-function App(){
-  return(
-    <>
-        <Login />
-
-       
-    </>
-     
-  )
-  
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<HomeInstrutor />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
